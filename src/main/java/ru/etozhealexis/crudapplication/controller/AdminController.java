c38f8f3d9d6fbb8e7ee1aaefce0629e9362dd911
etozhealexis <alexisworksya@yandex.ru>
2023-04-13 21:27:47 +0300

    rest frfrfrfr

--- src/main/java/ru/etozhealexis/crudapplication/controller/AdminController.java
+++ src/main/java/ru/etozhealexis/crudapplication/controller/AdminController.java
@@ -18,69 +18,69 @@ import java.util.List;
 @Controller()
 @RequestMapping("/admin")
 public class AdminController {
-    @Autowired
-    private UserService userService;
-
-    @Autowired
-    private RoleRepository roleRepository;
-
+//    @Autowired
+//    private UserService userService;
+//
+//    @Autowired
+//    private RoleRepository roleRepository;
+//
     @GetMapping
     public String listUsers(Model model, Authentication authentication) {
-        model.addAttribute("users", userService.getUsers());
+//        model.addAttribute("users", userService.getUsers());
         model.addAttribute("user", authentication.getPrincipal());
         model.addAttribute("roles", authentication.getAuthorities());
 
-        return "users";
+        return "admin";
     }
-
-    @GetMapping("/{id}")
-    public String listUser(@PathVariable("id") Long id, Model model) {
-        model.addAttribute("users", userService.getUser(id));
-
-        return "users";
-    }
-
-    @GetMapping("/new")
-    public String newUser(@ModelAttribute("user") User user) {
-        return "create";
-    }
-
-    @PostMapping
-    public String create(@ModelAttribute("user") @Valid User user,
-                         BindingResult bindingResult) {
-        if (bindingResult.hasErrors()) {
-            return "create";
-        }
-
-        userService.saveUser(user);
-
-        return "redirect:/admin";
-    }
-
-    @GetMapping("edit/{id}")
-    public String edit(@PathVariable("id") Long id, Model model) {
-        List<Role> roleList = roleRepository.findAll();
-        model.addAttribute("user", userService.getUser(id));
-        model.addAttribute("roleList", roleList);
-
-        return "edit";
-    }
-
-    @PatchMapping("/{id}")
-    public String update(@ModelAttribute("user") @Valid User user, BindingResult bindingResult,
-                         @PathVariable("id") Long id) {
-        if (bindingResult.hasErrors()) {
-            return "edit";
-        }
-
-        userService.update(id, user);
-        return "redirect:/admin";
-    }
-
-    @DeleteMapping("/{id}")
-    public String delete(@PathVariable("id") Long id) {
-        userService.delete(id);
-
-        return "redirect:/admin";
-    }
+//
+//    @GetMapping("/{id}")
+//    public String listUser(@PathVariable("id") Long id, Model model) {
+//        model.addAttribute("users", userService.getUser(id));
+//
+//        return "users";
+//    }
+//
+//    @GetMapping("/new")
+//    public String newUser(@ModelAttribute("user") User user) {
+//        return "create";
+//    }
+//
+//    @PostMapping
+//    public String create(@ModelAttribute("user") @Valid User user,
+//                         BindingResult bindingResult) {
+//        if (bindingResult.hasErrors()) {
+//            return "create";
+//        }
+//
+//        userService.saveUser(user);
+//
+//        return "redirect:/admin";
+//    }
+//
+//    @GetMapping("edit/{id}")
+//    public String edit(@PathVariable("id") Long id, Model model) {
+//        List<Role> roleList = roleRepository.findAll();
+//        model.addAttribute("user", userService.getUser(id));
+//        model.addAttribute("roleList", roleList);
+//
+//        return "edit";
+//    }
+//
+//    @PatchMapping("/{id}")
+//    public String update(@ModelAttribute("user") @Valid User user, BindingResult bindingResult,
+//                         @PathVariable("id") Long id) {
+//        if (bindingResult.hasErrors()) {
+//            return "edit";
+//        }
+//
+//        userService.update(id, user);
+//        return "redirect:/admin";
+//    }
+//
+//    @DeleteMapping("/{id}")
+//    public String delete(@PathVariable("id") Long id) {
+//        userService.delete(id);
+//
+//        return "redirect:/admin";
+//    }
 }
