9ef206a86288203ab661b2849752f9a6c9f1c8af
etozhealexis <alexisworksya@yandex.ru>
2023-04-14 21:31:59 +0300

    admin and user pages (no fetch for now)

---
+++ src/main/resources/static/scripts/admin.js
@@ -0,0 +1,25 @@
+window.onload = function () {
+
+}
+
+function createUser() {
+
+}
+
+function showUsersTable() {
+    alert('Users table');
+}
+
+function showAdminPanel() {
+    alert('admin');
+
+}
+
+function showUserPanel() {
+    alert('user');
+
+}
+
+console.log(1)
+document.getElementById("admin-panel-shower").addEventListener("click", showAdminPanel);
+document.getElementById("user-panel-shower").addEventListener("click", showUserPanel);
